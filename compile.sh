be573b8285b2ca2480eff718f191950fdaba9c5d
Niv Sulimany <sulimany.niv@campus.technion.ac.il>
2025-07-29 12:17:34 +0300

    Adding assymetric fence
    Shortened measurements time + add to compilation a membarrier JNI
    syscall
    
    Signed-off-by: Niv Sulimany <sulimany.niv@campus.technion.ac.il>

--- compile.sh
+++ compile.sh
@@ -1,23 +1,36 @@
 #!/bin/sh
 
-# Clean build directory and compile Java sources
-rm -rf build
-mkdir build
+set -e
 
-java="java"
-javac="javac"
-jar="jar"
+JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-21-openjdk-amd64}
+BUILD_DIR=build
+NATIVE_DIR=./nativeCode
+LIB_NAME=libmembarrier.so
+PACKAGE_MAIN=measurements.Main  # your main class
+JAR_NAME=experiments_instr.jar
 
-echo "COMPILING JAVA CLASSES..."
-cmd="$javac --add-exports java.base/jdk.internal.vm.annotation=ALL-UNNAMED -g -d build `find . -name *.java`"
-echo $cmd
-$cmd
+echo "Cleaning build directory..."
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
 
-cd build
+echo "Compiling Java sources from current dir and nativeCode/, generating JNI header into nativeCode/..."
+javac --add-exports java.base/jdk.internal.vm.annotation=ALL-UNNAMED \
+  -h "$NATIVE_DIR" -g -d "$BUILD_DIR" \
+  $(find . -name "*.java") $(find "$NATIVE_DIR" -name "*.java")
 
-if [ "$?" -eq "0" ]; then
-	echo "Main-class: measurements.Main" > manifest.mf
-	$jar cfm experiments_instr.jar manifest.mf *
-else
-	echo "ERROR compiling."
-fi
+echo "Compiling native JNI library..."
+gcc -fPIC -shared -o "$BUILD_DIR/$LIB_NAME" \
+  -I"$JAVA_HOME/include" \
+  -I"$JAVA_HOME/include/linux" \
+  "$NATIVE_DIR/membarrier.c"
+
+echo "Packaging classes into JAR..."
+cd "$BUILD_DIR"
+echo "Main-class: $PACKAGE_MAIN" > manifest.mf
+jar cfm "$JAR_NAME" manifest.mf $(find . -name "*.class")
+cd ..
+
+echo ""
+echo "✅ Build complete."
+echo "Run with:"
+echo "java -Djava.library.path=$BUILD_DIR -cp $BUILD_DIR/$JAR_NAME $PACKAGE_MAIN"
---
+++ nativeCode/membarrier.c
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include <linux/membarrier.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <errno.h>
+#include "nativecode_MemBarrier.h"
+
+JNIEXPORT jint JNICALL Java_nativeCode_MemBarrier_flushAllThreads(JNIEnv *env, jclass clazz) {
+    int ret = syscall(SYS_membarrier, MEMBARRIER_CMD_SHARED, 0);
+    if (ret == -1) {
+        return -errno;
+    }
+    return ret;
+}
